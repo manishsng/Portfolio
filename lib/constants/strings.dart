b34d0db3487806cfc8659da5814c7627ea240110
Manish Singh <manish.bk07@gmail.com>
2019-09-10 17:36:52 +0530

    Initial Commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,20 @@
+class Strings {
+  Strings._();
+
+  static const String about_me = 'About Me';
+  static const String about = 'About';
+  static const String me = ' Me';
+  static const String portfoli = 'Portfoli';
+  static const String o = 'o';
+  static const String headline = 'I\'am Manish Singh, front End Web Developer and Data Analyst From New Delhi, India.';
+  static const String summary = 'Focused Computer Science and Engineerign Student having excellent technical and communication skills, Proficient at analysing data to provide business solutions, designing and formulating test automation frameworks, writing code in various languages, feature development and implementation. Specialize in thinking outside the box to find unique solutions to difficult engineering problems.';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skill I Have';
+  static const String rights_reserved = '©️ KEEPING IT SOLID ALL RIGHTS REVSERVED';
+
+  // menu items
+  static const String menu_home = 'Home';
+  static const String menu_about = 'About';
+  static const String menu_contact = 'Contact';
+
+}
